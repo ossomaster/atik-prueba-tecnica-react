396df7a679a909fa0f9458eee4aa87177ca7d8a6
osso <fdantealvarezt@gmail.com>
2025-03-21 17:29:06 -0500

    deploy: configurando deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+	base: "/atik-prueba-tecnica-react",
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	optimizeDeps: {
+		exclude: ["lucide-react"],
+	},
+})
